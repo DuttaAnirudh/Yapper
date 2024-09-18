153f03a59594ca3a662ea042dab93528cf2a06f2
DuttaAnirudh <worktodutta@gmail.com>
2024-09-18 21:42:43 +0530

    add video conferencing  functionalities to meeting room

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,6 +1,12 @@
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
+import { Metadata } from "next";
 import { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "Yapper",
+  description: "A Video Conferencing App",
+};
 
 const HomeLayout = ({ children }: { children: ReactNode }) => {
   return (
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,11 @@
 import StreamVideoProvider from "@/providers/StreamClientProvider";
+import { Metadata } from "next";
 import { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "Yapper",
+  description: "A Video Conferencing App",
+};
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
