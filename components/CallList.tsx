1fe2f248681926c57462124e21bd60e8e99521f3
DuttaAnirudh <worktodutta@gmail.com>
2024-09-20 23:34:28 +0530

    render previous meetings and upcoming meetings information on their respective pages; display meetings recordings information on recordings page and allows users to see meeting recordings

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -3,14 +3,19 @@
 import useGetCalls from "@/hooks/useGetCalls";
 import { CallRecording } from "@stream-io/node-sdk";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MeetingCard from "./MeetingCard";
 import { Call } from "@stream-io/video-react-sdk";
+import Loader from "./Loader";
+import { useToast } from "@/hooks/use-toast";
+import { title } from "process";
 
 const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
   const [recordings, setRecordings] = useState<CallRecording[]>([]);
 
   const router = useRouter();
+
+  const { toast } = useToast();
 
   const {
     endedCalls,
@@ -25,7 +30,7 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
         return endedCalls;
 
       case "recordings":
-        return callRecordings;
+        return recordings;
 
       case "upcoming":
         return upcomingCalls;
@@ -49,13 +54,72 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
     }
   };
 
+  useEffect(() => {
+    const fetchRecording = async () => {
+      try {
+        const callData = await Promise.all(
+          callRecordings?.map((meeting) => meeting.queryRecordings()) ?? []
+        );
+
+        const recordings = callData
+          .filter((call) => call.recordings.length > 0)
+          .flatMap((call) => call.recordings);
+
+        setRecordings(recordings);
+      } catch (error) {
+        toast({ title: "Unable to fetch Recordings. Please Try Again Later" });
+      }
+    };
+
+    if (type === "recordings") {
+      fetchRecording();
+    }
+  }, [type, callRecordings, toast]);
+
   const calls = getCalls();
-  const noCallsMessage = getCalls();
+  const noCallsMessage = getNoCallsMessage();
+
+  if (isLoading) return <Loader />;
 
   return (
     <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
       {calls && calls.length > 0 ? (
-        calls.map((meeting: Call | CallRecording) => <MeetingCard />)
+        calls.map((meeting: Call | CallRecording) => (
+          <MeetingCard
+            key={(meeting as Call).id}
+            icon={
+              type === "ended"
+                ? "/icons/previous.svg"
+                : type === "upcoming"
+                ? "/icons/upcoming.svg"
+                : "/icons/recordings.svg"
+            }
+            title={
+              (meeting as Call).state?.custom.description.substring(0, 20) ||
+              (meeting as CallRecording).filename.substring(0, 20) ||
+              "No Description"
+            }
+            date={
+              (meeting as Call).state?.startsAt?.toLocaleString() ||
+              (meeting as CallRecording)?.start_time.toLocaleString()
+            }
+            buttonText={type === "recordings" ? "Play" : "Start"}
+            buttonIcon1={type === "recordings" ? "/icons/play.svg" : undefined}
+            isPreviousMeeting={type === "ended"}
+            link={
+              type === "recordings"
+                ? (meeting as CallRecording).url
+                : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${
+                    (meeting as Call).id
+                  }`
+            }
+            handleClick={
+              type === "recordings"
+                ? () => router.push(`/${(meeting as CallRecording).url}`)
+                : () => router.push(`/meeting/${(meeting as Call).id}`)
+            }
+          />
+        ))
       ) : (
         <h1>{noCallsMessage}</h1>
       )}
